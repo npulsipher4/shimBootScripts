e7b0719b09383806fc9ec0fbffdd11ad6c754c57
Nathan Pulsipher <107008636+npulsipher4@users.noreply.github.com>
2024-06-30 23:27:05 +0000

    Add files via upload

---
+++ shimDev.sh
@@ -0,0 +1,12 @@
+sudo growpart /dev/sda4
+sudo resize2fs sda4
+sudo apt update
+sudo apt install default-jdk python3 python3-pip
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
